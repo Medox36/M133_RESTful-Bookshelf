dffb7477369c4375f4db5ba87dc58478ce6c4f82
Medox36 <giuntinil@bzz.ch>
2022-05-11 12:43:30 +0200

    added javadoc for Publisher class

--- src/main/java/ch/bzz/bookshelf/model/Publisher.java
+++ src/main/java/ch/bzz/bookshelf/model/Publisher.java
@@ -2,6 +2,9 @@ package ch.bzz.bookshelf.model;
 
 import java.util.List;
 
+/**
+ * the publisher of one or more books
+ */
 public class Publisher {
     private String publisherUUID;
     private String publisher;
